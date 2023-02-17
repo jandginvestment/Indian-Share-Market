a281367a6e63c3cbbaa6e7e945d1ad94ca10091c
jandginvestment <jandginvestment@gmail.com>
2023-02-17 07:52:10 +0400

    update for running index
    need to compare same day to previous year close price

--- samedayLastyearAna.py
+++ samedayLastyearAna.py
@@ -6,21 +6,28 @@ import numpy as np
 ticker = "SBIN"
 df = get_history(symbol=ticker, start=pd.to_datetime('2010-01-01'), end=pd.to_datetime('2022-12-31'))
 
+df['Date'] = pd.to_datetime(df.index)
+
 # Create a new column that indicates the day of the year (1-365)
-df['day_of_year'] = df.index.dayofyear
+df['day_of_year'] = df['Date'].dt.dayofyear
 
 # Create a new column that indicates the year
-df['year'] = df.index.year
+df['year'] = df['Date'].dt.year
+
+df['running_index'] = df.groupby('year').cumcount()
+df['weekday'] = df['Date'].dt.day_name()
 
 # Create a new DataFrame that contains the close price of each stock on each day of the year, grouped by year
-comparedf = df.groupby(['day_of_year', 'year'])['Close'].mean().reset_index()
+comparedf = df.groupby(['running_index', 'year'])['Close'].mean().reset_index()
+
+comparedf = df[df['running_index'] < 250]
 
 # Get the close price of the stock on the same day of the last year
-last_year_close = comparedf[comparedf['year'] == 2022][comparedf['day_of_year'] == df.iloc[-1]['day_of_year']]['Close'].values[0]
+last_year_close = comparedf[comparedf['year'] == 2022][comparedf['running_index'] == comparedf.iloc[-1]['running_index']]['Close'].values[0]
 
 # Calculate the difference between the close price of the stock on the same day of the last year and the recent close price
 difference = df.iloc[-1]['Close'] - last_year_close
 
 # Print the result
 if difference > 0:
-    print(f"{ticker} has performed better this year than the same
+    print(f"{ticker} has performed better this year than the same")
